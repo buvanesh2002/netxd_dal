85a649d2426446ad40315f5f1d5230cec9ad05af
buvanesh2002 <buvanraj2002@gmail.com>
2023-08-31 12:03:35 +0530

    new

--- netxd_customer_dal/netxd_customer_dal_interfaces/interfaces.go
+++ netxd_customer_dal/netxd_customer_dal_interfaces/interfaces.go
@@ -2,8 +2,6 @@ package netxdcustomerdalinterfaces
 
 import netxdcustomerdalmodels "github.com/buvanesh2002/netxd_dal/netxd_customer_dal/netxd_customer_dal_models"
 
-
-
 type ICustomer interface {
 	CreateCustomer(user *netxdcustomerdalmodels.Customer) (*netxdcustomerdalmodels.CustomerResponse, error)
 }
